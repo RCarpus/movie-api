d64e6054c3499262da3b9dec59e9d90309504771
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-11-22 14:35:15 -0500

    updated app.listen

--- index.js
+++ index.js
@@ -251,6 +251,7 @@ app.use((err, req, res, next) => {
 });
 
 // open server
-app.listen(8080, () => {
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
   console.log('Your app is listening on port 8080.');
 });
