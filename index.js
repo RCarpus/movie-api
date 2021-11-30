6aabf6fed500bc61e8fe58064862dbc17263a935
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-11-30 08:47:35 -0500

    udpate cors policy

--- index.js
+++ index.js
@@ -22,20 +22,20 @@ app.use(bodyParser.json());
 let auth = require('./auth')(app); //This needs to come after bodyParser
 
 const cors = require('cors');
-app.use(cors()); // allows access from all origins
+// app.use(cors()); // allows access from all origins
 /* This would restrict origins */
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
 
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){
-//       let message = 'The CORS policy for this application doesn\'t allow access from the origin ' + origin;
-//       return callback(new Error(message), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      let message = 'The CORS policy for this application doesn\'t allow access from the origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // logging middleware
 app.use(morgan('common'));
