30550e2ef7d3a8b4775491de52ae80623d2ff3f9
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-11-30 09:39:30 -0500

    added back in authentication for /movies

--- index.js
+++ index.js
@@ -45,8 +45,7 @@ app.use(morgan('common'));
 app.use(express.static('public'));
 
 // endpoint to get all movies
-//app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
